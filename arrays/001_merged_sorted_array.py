6ae2e4c3aa4d128118ebf8000e8bd7cfc6ecbd78
caro-cli <carolinemmen@gmail.com>
2024-01-24 17:48:41 -0600

    solution plus walkthrough

--- arrays/001_merged_sorted_array.py
+++ arrays/001_merged_sorted_array.py
@@ -25,14 +25,96 @@ Output: [1]
 Explanation: The arrays we are merging are [] and [1].
 The result of the merge is [1].
 Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
-
-
-Notes:
-we need to look at one array and compare the current number to the current number in the second array. they're both sorted in ascending order. nums 1 will have extra 0s which is to accomodate for the length of the second array.
-
-if nums1[0] is < nums2[0], then we're good.
-if nums[1] < nums2[0], we need to do another loop. if nums[1] > nums[2], then we'd need to store the nums[1] num in another variable, replace the nums[1] num with the nums[2] number.
 """
 
 def merge(nums1, m, nums2, n):
-  pass
+  mpointer, npointer, last = m - 1, n - 1, m + n -1
+
+  while npointer >= 0:
+    if mpointer >= 0 and nums1[mpointer] > nums2[npointer]:
+      nums1[last] = nums1[mpointer]
+      mpointer -= 1
+    else:
+      nums1[last] = nums2[npointer]
+      npointer -= 1
+
+    last -= 1
+
+# nums1 = [1,2,3,0,0,0]
+# m = 3
+# nums2 = [2,5,6]
+# n = 3
+
+# mpointer = 3 - 1 = 2
+# npointer = 3 - 1 = 2
+# last = 3 + 3 - 1 = 5 <- gets us the last index
+
+# first loop:
+#         (2)
+# while npointer >= 0:
+#                 (3)       (6)
+#   if 2 >= 0 and nums1[2] > nums2[2]:
+#     nums1[5] = nums1[2]
+#     mpointer = 3 - 1 = 2
+#   else:
+#                 (6)
+#     nums1[5] = nums2[2] <- we hit this
+#     npointer = 3 - 1 = 2
+# last = 4
+
+# nums1 should now be [1,2,3,0,0,6]
+# npointer is now 1
+# mpointer is still 2
+# last is now 4
+
+# second loop:
+#       (1)
+# while npointer >= 0:
+#               (3)           (5)
+#   if 2 >= 0 and nums1[2] > nums2[1]:
+#     nums1[4] = nums1[2] (which is 3)
+#     mpointer -= 1
+#   else:
+#                 (5)
+#     nums1[4] = nums2[1] <- we hit this
+#     npointer = 1 - 1 = 0
+# last = 3
+
+# nums1 should now be [1,2,3,0,5,6]
+# npointer is now 0
+# mpointer is still 2
+# last is now 3
+
+# third loop
+#         (0)
+# while npointer >= 0:
+#                 (3)       (2)
+# if 2 >= 9 and nums1[2] > nums2[0]:
+#   nums1[3] = nums1[2] (which is 3) <- we hit this
+#   mpointer -= 1 (2 - 1) = 1
+# else:
+#   nums1[3] = nums2[0]
+#   npointer -=1
+# last = 2
+
+# nums1 should now be [1,2,3,3,5,6]
+# npointer is still 0
+# mpointer is now 1
+# last is now 2
+
+# fourth loop
+#         (0)
+# while npointer >= 0:
+#               (2)         (2)
+# if 1 >= 0 and nums1[1] > nums2[0]:
+#   nums1[2] = nums1[1]
+# else:
+#   nums1[2] = nums2[0] (which is 2) <- we hit this
+#   npointer -= 1 (so it's now -1)
+# last = 1
+
+# nums1 is now [1,2,2,3,5,6]
+# npointer is now -1
+# mpointer is still 1
+# last is now 1
+# we no longer go into the while loop
