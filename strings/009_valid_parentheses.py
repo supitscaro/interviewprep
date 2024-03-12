3b2c78d2b5f3e08c94fca75a9a76a0378566cbe7
thisiscaro <carolinemmen@gmail.com>
2024-03-11 21:01:45 -0500

    first commit

--- strings/009_valid_parentheses.py
+++ strings/009_valid_parentheses.py
@@ -20,4 +20,40 @@ Example 3:
 
 Input: s = "(]"
 Output: false
+
+Notes:
+we can use a hash map to store which bracket belongs to which bracket:
+brackets = {
+ "}": "{",
+ "]": "[",
+ ")": "("
+}
+
+I know a stack could potentially be helpful here but trying to figure out why.
+I could loop through the string and add each bracket into the stack but other than taking up space
+it doesn't do much.
+
+We could check if the current bracket we're in is in the brackets hash. and if it is, just remove it from the stack.
+We could maybe check if the length of the stack is even but that doesn't actually take into account the matching pairs. 
+We could though check if the length of s is even before doing any of the other logic, cause if it's not we can just return false
+
+But back to the loop. Maybe we check if the current bracket is in the hash. If it's not, then add it to the stack. If it is, then 
+we do a .pop()
 """
+
+
+def isValid(s):
+    if s % 2 != 0:
+        return False
+
+    stack = []
+    bracket_hash = {"}": "{", "]": "[", ")": "("}
+
+    for bracket in s:
+        if bracket in bracket_hash:
+            stack.pop()
+        else:
+            stack.append(bracket)
+
+    if len(stack) == 0:
+        return True
