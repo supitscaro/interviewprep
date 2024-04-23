67141f1c40ee852eab4ab688796ee77c628a564a
thisiscaro <carolinemmen@gmail.com>
2024-04-23 18:47:16 -0500

    fix logic

--- strings/009_valid_parentheses.py
+++ strings/009_valid_parentheses.py
@@ -6,7 +6,7 @@ An input string is valid if:
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
- 
+
 
 Example 1:
 
@@ -34,10 +34,10 @@ I could loop through the string and add each bracket into the stack but other th
 it doesn't do much.
 
 We could check if the current bracket we're in is in the brackets hash. and if it is, just remove it from the stack.
-We could maybe check if the length of the stack is even but that doesn't actually take into account the matching pairs. 
+We could maybe check if the length of the stack is even but that doesn't actually take into account the matching pairs.
 We could though check if the length of s is even before doing any of the other logic, cause if it's not we can just return false
 
-But back to the loop. Maybe we check if the current bracket is in the hash. If it's not, then add it to the stack. If it is, then 
+But back to the loop. Maybe we check if the current bracket is in the hash. If it's not, then add it to the stack. If it is, then
 we do a .pop()
 """
 
@@ -51,7 +51,10 @@ def isValid(s):
 
     for bracket in s:
         if bracket in bracket_hash:
-            stack.pop()
+            if stack and stack[-1] == bracket_hash[bracket]:
+                stack.pop()
+            else:
+                return False
         else:
             stack.append(bracket)
 
