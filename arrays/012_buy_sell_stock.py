d878ef4ad66c62d0c5df63813f3781b92e549d6e
thisiscaro <carolinemmen@gmail.com>
2024-04-24 22:12:35 -0500

    solve buy/sell stock

--- arrays/012_buy_sell_stock.py
+++ arrays/012_buy_sell_stock.py
@@ -41,4 +41,18 @@ figure out one part of the problem and it's not even a full solution
 
 
 def maxProfit(prices):
-    pass
+    buy, sell = 0, 1
+
+    max_profit = 0
+
+    while sell < len(prices):
+        # we have some profit
+        if prices[buy] < prices[sell]:
+            profit = prices[sell] - prices[buy]
+            max_profit = max(max_profit, profit)
+        else:
+            buy = sell
+
+        sell += 1
+
+    return max_profit
